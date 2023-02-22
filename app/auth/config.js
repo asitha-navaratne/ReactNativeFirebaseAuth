c73a355b386443f03bc09ee5babbfa4e36d18cb3
Asitha Navaratne <asithan476@gmail.com>
2023-02-22 23:24:42 +0530

    Added async-storage for Firebase and fixed minor bug in app.json

--- app/auth/config.js
+++ app/auth/config.js
@@ -1,7 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  initializeAuth,
+  getReactNativePersistence,
+} from "firebase/auth/react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import {
   FIREBASE_API_KEY,
@@ -24,3 +26,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = initializeAuth(app, {
+  persistance: getReactNativePersistence(AsyncStorage),
+});
