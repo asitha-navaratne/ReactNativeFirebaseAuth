1fafa4db8d1298f37e2586ae8125f7336c5abae0
Asitha Navaratne <asithan476@gmail.com>
2023-02-23 14:20:17 +0530

    Completed SignIn and SignUp screens

--- app/screens/authScreens/SignInScreen.jsx
+++ app/screens/authScreens/SignInScreen.jsx
@@ -1,11 +1,83 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+import { useAuth } from "../../contexts/AuthContext";
 
 const SignInScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { handleSignIn } = useAuth();
+
   return (
-    <View>
-      <Text>Sign In Page</Text>
-    </View>
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={() => handleSignIn(email, password)}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputContainer: {
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: "#0782f9",
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+  },
+});
 
 export default SignInScreen;
--- app/screens/authScreens/SignUpScreen.jsx
+++ app/screens/authScreens/SignUpScreen.jsx
@@ -1,11 +1,84 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Button, TextInput } from "react-native-paper";
+
+import { useAuth } from "../../contexts/AuthContext";
 
 const SignUpScreen = () => {
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+
+  const { handleSignUp } = useAuth();
+
   return (
-    <View>
-      <Text>Sign Up Page</Text>
+    <View style={styles.container} behaviour="padding">
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="First Name"
+          value={user["firstName"]}
+          onChangeText={(text) =>
+            setUser((prev) => ({ ...prev, firstName: text }))
+          }
+        />
+        <TextInput
+          placeholder="Last Name"
+          value={user["lastName"]}
+          onChangeText={(text) =>
+            setUser((prev) => ({ ...prev, lastName: text }))
+          }
+        />
+        <TextInput
+          placeholder="Email"
+          value={user["email"]}
+          onChangeText={(text) => setUser((prev) => ({ ...prev, email: text }))}
+        />
+        <TextInput
+          placeholder="Phone"
+          value={user["phone"]}
+          keyboardType="phone-pad"
+          maxLength={9}
+          onChangeText={(text) => setUser((prev) => ({ ...prev, phone: text }))}
+          mode="outlined"
+          left={<TextInput.Affix text="+94" />}
+        />
+        <TextInput
+          placeholder="Password"
+          value={user["password"]}
+          onChangeText={(text) =>
+            setUser((prev) => ({ ...prev, password: text }))
+          }
+          secureTextEntry
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button mode="contained" onPress={() => handleSignUp(user)}>
+          Register
+        </Button>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputContainer: {
+    width: "80%",
+  },
+  buttonContainer: {
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
+});
 
 export default SignUpScreen;
