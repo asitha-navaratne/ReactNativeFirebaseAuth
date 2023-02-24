9ef949125aaf13bb47cef6a05769da4c92f7a19f
Asitha Navaratne <asithan476@gmail.com>
2023-02-24 23:53:10 +0530

    Loading and Home pages complete

--- app/screens/appScreens/HomeScreen.jsx
+++ app/screens/appScreens/HomeScreen.jsx
@@ -1,11 +1,52 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import { useAuth } from "../../contexts/AuthContext";
 
 const HomeScreen = () => {
+  const { authData, handleSignOut } = useAuth();
+
   return (
-    <View>
-      <Text>Home Page</Text>
+    <View style={styles.container}>
+      <View style={styles.info}>
+        <Text>User ID: {authData.uid}</Text>
+        <Text>
+          Name: {authData.firstName}&nbsp;{authData.lastName}
+        </Text>
+        <Text>Email: {authData.email}</Text>
+        <Text>Phone: {authData.phone}</Text>
+        <Text>User ID: {authData.uid}</Text>
+      </View>
+      <TouchableOpacity style={styles.button} onPress={handleSignOut}>
+        <Text style={styles.buttonText}>Sign Out</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  info: {
+    borderColor: "#aaa",
+    borderWidth: 5,
+    borderRadius: 5,
+    padding: 11,
+  },
+  button: {
+    backgroundColor: "#0782f9",
+    width: "60%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+    marginTop: 40,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+  },
+});
 
 export default HomeScreen;
--- app/screens/appScreens/LoadingScreen.jsx
+++ app/screens/appScreens/LoadingScreen.jsx
@@ -1,11 +1,19 @@
-import { Text, View } from "react-native";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 
 const LoadingScreen = () => {
   return (
-    <View>
-      <Text>Loading Page</Text>
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color="#f28b3d" />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default LoadingScreen;
