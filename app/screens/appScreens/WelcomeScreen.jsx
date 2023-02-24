3615c1824afd4b113360069469a3ab7f92fae5cd
Asitha Navaratne <asithan476@gmail.com>
2023-02-24 23:36:36 +0530

    Welcome Screen complete

--- app/screens/appScreens/WelcomeScreen.jsx
+++ app/screens/appScreens/WelcomeScreen.jsx
@@ -1,11 +1,37 @@
-import { Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Button } from "react-native-paper";
 
-const WelcomeScreen = () => {
+const WelcomeScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>Welcome Screen</Text>
+    <View style={styles.container} behaviour="padding">
+      <View>
+        <View style={styles.buttonContainer}>
+          <Button onPress={() => navigation.navigate("Login")} mode="contained">
+            Login
+          </Button>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={() => navigation.navigate("Register")}
+            mode="outlined"
+          >
+            Register
+          </Button>
+        </View>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    marginVertical: 5,
+  },
+});
 
 export default WelcomeScreen;
